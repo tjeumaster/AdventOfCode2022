07d817c981fe0f1f88459d8bd05ba661be2d8a77
tjeumaster <mathieu.dujardin@outlook.com>
2022-12-04 22:35:04 +0100

    final

---
+++ Day4_CampCleanup/Assignment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_CampCleanup
+{
+    internal class Assignment
+    {
+        private int _startPosition;
+        private int _endPosition;
+
+        public Assignment(string assignment)
+        {
+            var positions = assignment.Split('-');
+            _startPosition = int.Parse(positions[0]);
+            _endPosition = int.Parse(positions[1]);
+        }
+
+        public bool Contains(Assignment otherPairAssignment)
+        {
+            return _startPosition <= otherPairAssignment._startPosition && _endPosition >= otherPairAssignment._endPosition;
+        }
+
+        public bool Overlaps(Assignment otherPairAssignment)
+        {
+            return _endPosition >= otherPairAssignment._startPosition && otherPairAssignment._endPosition >= _startPosition;
+        }
+    }
+}
---
+++ Day4_CampCleanup/Program.cs
@@ -0,0 +1,30 @@
+﻿using Day4_CampCleanup;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\dujardm\\Desktop\\Advent Of Code 2022\\Day4_CampCleanup\\input.txt";
+        string[] lines = File.ReadAllLines(filePath);
+
+        int containsCount = 0;
+        int overlapsCount = 0;
+
+        foreach (var pair in lines)
+        {
+            var assignments = pair.Split(",");
+
+            var assignment1 = new Assignment(assignments[0]);
+            var assignment2 = new Assignment(assignments[1]);
+
+            if (assignment1.Contains(assignment2) || assignment2.Contains(assignment1))
+                containsCount++;
+
+            if (assignment1.Overlaps(assignment2))
+                overlapsCount++;
+        }
+
+        Console.WriteLine(containsCount);
+        Console.WriteLine(overlapsCount);
+    }
+}
